636f5141f552e73bda5d8affa519bc048b23a438
Ícaro Emannuel <emannuelalmeida@gmail.com>
2021-01-04 22:07:23 -0300

    Set Player basic movement and add gravity

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var translation = ProcessInput() * Time.deltaTime;
+        transform.Translate(translation);
+    }
+
+    private Vector3 ProcessInput() 
+    {
+        Vector3 direction = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector3.back;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector3.right;
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            direction *= 5;
+        }
+
+        return direction;
+    }
+}
